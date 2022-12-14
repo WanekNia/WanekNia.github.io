154aa51b7baa8d9bd9f2278d800dab347a5b9aae
nathan wanecque <nathan.wanecque@outlook.com>
2022-12-14 14:53:57 +0100

    ...

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'WanekNia', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 1, // How many projects to display.
+    limit: 3, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
@@ -12,12 +12,6 @@ const config = {
   },
   social: {
     linkedin: 'Nathan Wanecque',
-    twitter: 'NO!',
-    facebook: 'not have',
-    instagram: 'nope',
-    dribbble: 'nope',
-    behance: 'nope',
-    medium: 'WanekNia',
     dev: 'WanekNia',
     stackoverflow: '', // format: userid/username
     website: 'nope',
@@ -29,20 +23,10 @@ const config = {
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'C++',
+    'C#',
+    'Unreal Engine',
+    '',
   ],
   experiences: [
     {
@@ -70,38 +54,26 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Ynov',
+      degree: 'Bachelor',
+      from: '2022',
+      to: '2023',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: 'medium', // medium | dev
+    username: 'WanekNia', // to hide blog section, keep it empty
     limit: 3, // How many posts to display. Max is 10.
   },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
+  
   // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
-  },
   themeConfig: {
     defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
