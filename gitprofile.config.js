8a0c10a357470cc65ce371e912cfc44cf3946276
nathan wanecque <nathan.wanecque@outlook.com>
2022-12-14 15:24:15 +0100

    add CV link

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -20,7 +20,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/WanekNia/WanekNia.github.io/blob/main/CV_Wanek.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'C++',
